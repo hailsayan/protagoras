0287e4d9b04b218ad36eaac5abad7f489106f60f
Psyon <sayan.h2020@gmail.com>
2024-12-27 22:54:04 +0330

    add 18

---
+++ partOne/18-tiredShepherd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriterSize(os.Stdout, math.MaxInt32)
+
+func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
+
+func main() {
+	var n int
+	scanf("%d\n", &n)
+
+	f := n / 4
+
+	printf("%d\n", f)
+	writer.Flush()
+}
